35c66215e93d27d9af8c47555f0a68ffcc09dd86
PerGraa <github.com@gybe.dk>
2017-12-02 22:33:16 +0300

    Minor cleanup

--- src/server.cpp
+++ src/server.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 #include <thread>
 
+#include "server/store_endpoint.hpp"
 #include "server/store.hpp"
 #include "server/simple_response.hpp"
-#include "server/store_endpoint.hpp"
 
 int main() {
   using namespace kvstore;  // NOLINT
 
   // This call will not return in the normal case.
   // Kill server with Ctrl+C in terminal.
-  //  start_rest_server<Store, SimpleResponse>();
   start_rest_server<Store, SimpleResponse>();
 
   return EXIT_SUCCESS;
