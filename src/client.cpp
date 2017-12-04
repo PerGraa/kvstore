499b578b9e2da05f561ccf0ef01d1b0c68bd6501
PerGraa <github.com@gybe.dk>
2017-12-04 07:47:23 +0300

    Better client

--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,8 @@
-#include <mutex>
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <mutex>
+#include <sstream>
 
 // Silence Clang compiler warnings
 #pragma clang diagnostic push
@@ -14,27 +15,26 @@
 
 #pragma clang diagnostic pop
 
-/** Thread safe cout class
-  * Exemple of use:
-  *    PrintThread{} << "Hello world!" << std::endl;
-  */
+// Thread safe cout class
+// Example of use: PrintThread{} << "Hello world!" << std::endl;
 class PrintThread : public std::ostringstream {
  public:
   PrintThread() = default;
 
   ~PrintThread() {
-    std::lock_guard<std::mutex> guard(_mutexPrint);
+    std::lock_guard<std::mutex> guard(g_mutex_print);
     std::cout << this->str();
   }
 
  private:
-  static std::mutex _mutexPrint;
+  static std::mutex g_mutex_print;
 };
 
-std::mutex PrintThread::_mutexPrint{};
+std::mutex PrintThread::g_mutex_print{};
 
-void send_request(int thread_id, int number_of_messages,
-                  std::chrono::milliseconds millisecs) {
+void send_request(int thread_id, int number_of_messages, int number_of_keys, int ms) {
+  std::chrono::milliseconds millisecs(ms);
+
   UrlRequest request;
   request.host("0.0.0.0");
   request.port(8888);
@@ -44,23 +44,28 @@ void send_request(int thread_id, int number_of_messages,
 
   std::random_device random_device;
   std::mt19937 engine{random_device()};
-  std::uniform_int_distribution<int> dist(1, 20);
+  std::uniform_int_distribution<int> dist_discrete(1, 4 * number_of_keys);
   std::uniform_real_distribution<> dist_real(0.75, 1.25);
 
+  auto get_random_key = [&]() {
+    return std::to_string(dist_discrete(engine) % number_of_keys);
+  };
+
   for (int i = 0; i < number_of_messages; ++i) {
-    switch (dist(engine) % 4) {
+    // Select one of the four rest methods evenly.
+    switch (dist_discrete(engine) % 4) {
       case 0: {
         request.method("PUT");
-        request.uri("/put/key/" + std::to_string(dist(engine) % 10) + "/value/" +
-                    std::to_string(i) + "_thread" + std::to_string(thread_id));
+        request.uri("/put/key/" + get_random_key() + "/value/" + std::to_string(i) +
+                    "_thread" + std::to_string(thread_id));
       } break;
       case 1: {
         request.method("DELETE");
-        request.uri("/delete/key/" + std::to_string(dist(engine) % 10));
+        request.uri("/delete/key/" + get_random_key());
       } break;
       case 2: {
         request.method("GET");
-        request.uri("/get/key/" + std::to_string(dist(engine) % 10));
+        request.uri("/get/key/" + get_random_key());
       } break;
       case 3: {
         request.method("GET");
@@ -69,37 +74,71 @@ void send_request(int thread_id, int number_of_messages,
 
       default: { } break; }
 
+    // Wait for response or timeout
     auto response = request.perform();
 
-    // TODO print if debug set
-    if (true or response.statusCode() == 408) {
-      PrintThread{} << "Thread[" << thread_id << "] MsgID[" << i << thread_id
-                    << "] Status[" << response.statusCode()
-                    << "] Body: " << response.body();
-    }
+    // These prints with mutex adds wall time for running client.
+    // Linux on older laptop:       4-5% extra time
+    // Linux in VM on newer laptop:
+    PrintThread{} << "Thread[" << thread_id << "] MsgID[" << i << thread_id
+                  << "] Status[" << response.statusCode()
+                  << "] Body: " << response.body()
+                  << (response.statusCode() == 408 ? "\n" : "");
 
+    // Sleep a bit
     std::this_thread::sleep_for(millisecs * dist_real(engine));
   }
 }
 
-int main() {
-  std::cout << "Starting REST client\n";
+bool check_input(char* input, int& target, std::string name, std::string usage) {
+  std::istringstream ss(input);
+  target = 0;
 
-  std::vector<std::thread> vt;
-  constexpr int number_of_messages = 2000;             // TODO >= 1
-  constexpr int number_of_threads  = 10;               // TODO >= 1
-  constexpr std::chrono::milliseconds millisecs(250);  // TODO >= 1
+  if (!(ss >> target) || target < 1) {
+    std::cerr << usage;
+    std::cerr << name << " must be > 0\n";
+    return false;
+  }
+  return true;
+}
 
-  for (int i = 0; i < number_of_threads; ++i) {
-    vt.emplace_back(std::thread{send_request, i, number_of_messages, millisecs});
+int main(int argc, char** argv) {
+  int number_of_threads  = 0;
+  int number_of_keys     = 0;
+  int number_of_messages = 0;
+  int millisecs          = 0;
+  std::string usage =
+      "client <number_of_threads> <number_of_keys> <messages_per_thread> "
+      "<sleep_in_milliseconds>\nExample: client 2 10 20 500\n";
+
+  if (argc != 5) {
+    std::cerr << usage;
+    std::cerr << "Wrong parameters given\n";
+    return EXIT_FAILURE;
   }
 
-  for (auto &t : vt) {
+  if ((!check_input(argv[1], number_of_threads, "number_of_threads", usage)) ||
+      (!check_input(argv[2], number_of_keys, "number_of_keys", usage)) ||
+      (!check_input(argv[3], number_of_messages, "messages_per_thread", usage)) ||
+      (!check_input(argv[4], millisecs, "sleep_in_milliseconds", usage))) {
+    return EXIT_FAILURE;
+  }
+
+  std::vector<std::thread> vt;
+
+  std::cout << "Sending messages\n";
+
+  for (int i = 0; i < number_of_threads; ++i) {
+    // Start threads
+    vt.emplace_back(
+        std::thread{send_request, i, number_of_messages, number_of_keys, millisecs});
+  }
+
+  // Wait for all threads to finish
+  for (auto& t : vt) {
     t.join();
   }
 
-  std::cout << "Threads[" << number_of_threads << "]\nTotal messages["
-            << number_of_threads * number_of_messages << "]\nSleep interval[~"
-            << millisecs.count() << "ms]\n";
+  std::cout << "Done. Client exiting\n";
   return EXIT_SUCCESS;
 }
